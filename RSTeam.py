c4c330feaadd75a2c5abb6b4afb69267e2104e9b
REDSEC <68374733+hjprihatini@users.noreply.github.com>
2022-02-12 21:39:08 +0700

    Add files via upload

---
+++ RSTeam.py
@@ -0,0 +1,31 @@
+import time
+import os
+import sys
+
+os.system("clear")
+os.system("figlet RSTeamn Tools")
+print("===================================================")
+print("Creator : RedSec")
+print("Team : RSTeam")
+print("===================================================")
+print
+print("[1] RedHawk")
+print("[2] Track Ip")
+print("[3] Vbug Virus Maker")
+print("[4] WPscan (Kali Linux)")
+print("[5] SET (Kali Linux)")
+print
+pilih=input('[Pilih Menu Diatas]==> ')
+print
+if pilih == 1:
+    os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK.git")
+if pilih == 2:
+    os.system*("git clone https://github.com/htr-tech/track-ip.git")
+if pilih == 3:
+    os.system("git clone https://github.com/Gameye98/vbug.git")
+if pilih == 4:
+    os.system("sudo apt-get install wpscan")
+    os.system("sudo wpscan")
+if pilih == 5:
+    os.system("sudo apt-get install set")
+    os.system("sudo set")
